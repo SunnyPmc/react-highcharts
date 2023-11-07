69209068d9049b120a9c09d7dd56e81f1f0232d5
SunnyPmc <102278349+SunnyPmc@users.noreply.github.com>
2023-11-07 06:48:29 +0545

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import HighCharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const options = {
+  series: [
+    {
+      name: 'Profit',
+      data: [100, 200, 30, 100, 30, 50, 60]
+    }
+  ]
 }
 
-export default App;
+ class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <HighchartsReact highcharts={HighCharts} options={options}></HighchartsReact>
+      </div>
+    )
+  }
+}
+
+export default App
